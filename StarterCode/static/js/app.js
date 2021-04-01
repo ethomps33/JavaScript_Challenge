f75ab162d2189ccfae8a8fcff0eeea97987f2581
ethomps33 <e.thomps33@gmail.com>
2021-03-31 17:00:40 -0700

    Created a table that will populate to the html on the webpage.Created a function to pull data from data file to populate it in the table by the date entered.

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,30 @@
+// from data.js
+var tableData = data;
+//selecting the area of HTML that you want to create the 
+var table = d3.select("tbody")
+
+function make_table(data) {
+    table.html("")
+    data.forEach((table_row) => {
+        var row = table.append("tr");
+        Object.values(table_row).forEach((val) => {
+            var cell = row.append("td");
+            cell.text(val)
+        });
+    }
+    );
+};
+function on_click() {
+var input_date = d3.selct("#datetime").property("value");
+
+var new_data = tableData;
+var new_data = new_data.filter(table_row => table_row.datetime == input_date);
+
+make_table(new_data)
+
+};
+d3.select("#filter-btn").on("click", on_click)
+d3.select("#filter-btn").on("enter", on_click)
+make_table(tableData);
+
+
